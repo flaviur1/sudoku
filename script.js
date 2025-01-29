911568dc9d3fb02755a0ee19dd546ce3902b250f
Romitan Flaviu <flaviu.romitan23@gmail.com>
2025-01-29 23:55:10 +0200

    changed the grid to 3x3 so it is standard, changed the ids of the cells to be in the correct format now, started the indexing in table from 0 instead of 1, started work on a function that maps our table to our cells

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const createEmptyTable = (size) => {
   // table will be an object that has { row : {c1: val, c2 : val, ...}, }
   table = {};
-  for (let i = 1; i <= size; i++) {
+  for (let i = 0; i < size; i++) {
     table[i] = {};
   }
   return table;
 };
 
 const checkNumber = (table, row, col, size) => {
-  for (let i = 1; i <= size; i++) {
+  for (let i = 0; i < size; i++) {
     // we will check both the row and the col in the same iteration
     if (i != row) {
       if (table[i][col] == table[row][col]) return false;
@@ -21,22 +21,35 @@ const checkNumber = (table, row, col, size) => {
   return true;
 };
 
-table = createEmptyTable(6);
-table[1][1] = 1;
-table[1][2] = 2;
-table[1][3] = 3;
-table[1][4] = 4;
-table[1][5] = 5;
-table[1][6] = 6;
-table[1][7] = 7;
-table[1][8] = 8;
-table[1][9] = 9;
+const transferNumbers = (table, size) => {
+  for (let i = 1; i < size * size; i++) {
+    let row = Math.floor(i / size);
+    let col = i % size;
+    console.log("row " + row + " col " + col);
+    if (table[row] != undefined && table[row][col] != undefined) {
+      document.getElementById(i).innerHTML = table[row][col];
 
-let str = "1";
+    }
+  }
+};
 
-document.getElementById(str).innerHTML = table[1][1];
+table = createEmptyTable(9);
+table[0][0] = 1;
+table[0][1] = 2;
+table[0][2] = 3;
+table[0][3] = 4;
+table[0][4] = 5;
+table[0][5] = 6;
+table[0][6] = 7;
+table[0][7] = 8;
+table[0][8] = 9;
+
+let str = "57";
+
+document.getElementById(str).innerHTML = table[0][0];
 
 console.log(table);
-console.log(checkNumber(table, 1, 1, 6));
-table[1][6] = 1;
-console.log(checkNumber(table, 1, 1, 6));
+console.log(checkNumber(table, 0, 0, 9));
+table[0][5] = 1;
+console.log(checkNumber(table, 0, 0, 9));
+transferNumbers(table, 9);
