aea4837bfddf639634184f760fbf7acce5bef2ff
Romitan Flaviu <flaviu.romitan23@gmail.com>
2025-01-29 21:46:41 +0200

    created a function checkNumber to check the validity of a number from the table, added id in html to all the cells to be able to modify them from the script using document.getElementById

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
-const createEmptyTable = (nrRows) => {
+const createEmptyTable = (size) => {
   // table will be an object that has { row : {c1: val, c2 : val, ...}, }
   table = {};
-  for (let i = 1; i <= nrRows; i++) {
+  for (let i = 1; i <= size; i++) {
     table[i] = {};
   }
   return table;
 };
 
-function checkNumber(table) {}
+const checkNumber = (table, row, col, size) => {
+  for (let i = 1; i <= size; i++) {
+    // we will check both the row and the col in the same iteration
+    if (i != row) {
+      if (table[i][col] == table[row][col]) return false;
+    }
 
-console.log(createEmptyTable(6));
+    if (i != col) {
+      if (table[row][i] == table[row][col]) return false;
+    }
+  }
+  return true;
+};
+
+table = createEmptyTable(6);
+table[1][1] = 1;
+table[1][2] = 2;
+table[1][3] = 3;
+table[1][4] = 4;
+table[1][5] = 5;
+table[1][6] = 6;
+table[1][7] = 7;
+table[1][8] = 8;
+table[1][9] = 9;
+
+let str = "1";
+
+document.getElementById(str).innerHTML = table[1][1];
+
+console.log(table);
+console.log(checkNumber(table, 1, 1, 6));
+table[1][6] = 1;
+console.log(checkNumber(table, 1, 1, 6));
