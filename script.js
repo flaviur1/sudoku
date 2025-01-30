faa0049594f5121e425f9dfa2960fbab0647146c
Romitan Flaviu <flaviu.romitan23@gmail.com>
2025-01-30 23:16:29 +0200

    changed the ids of the cells to start from 0 instead of 1, changed the name of the transferTable function to displayTable, changed the formula to calculate the correct coloumn now, started a function to create a game table

--- script.js
+++ script.js
@@ -21,35 +21,22 @@ const checkNumber = (table, row, col, size) => {
   return true;
 };
 
-const transferNumbers = (table, size) => {
-  for (let i = 1; i < size * size; i++) {
+const displayTable = (table, size) => {
+  for (let i = 0; i < size * size; i++) {
     let row = Math.floor(i / size);
-    let col = i % size;
-    console.log("row " + row + " col " + col);
+    let col = i - row * 9;
     if (table[row] != undefined && table[row][col] != undefined) {
       document.getElementById(i).innerHTML = table[row][col];
-
     }
   }
 };
 
+const createTable = (table, size) => {
+  for (let i = 0; i < size * size; i++) {
+    let row = Math.floor(i / size);
+    let col = i - row * 9;
+  }
+};
+
 table = createEmptyTable(9);
-table[0][0] = 1;
-table[0][1] = 2;
-table[0][2] = 3;
-table[0][3] = 4;
-table[0][4] = 5;
-table[0][5] = 6;
-table[0][6] = 7;
-table[0][7] = 8;
-table[0][8] = 9;
-
-let str = "57";
-
-document.getElementById(str).innerHTML = table[0][0];
-
-console.log(table);
-console.log(checkNumber(table, 0, 0, 9));
-table[0][5] = 1;
-console.log(checkNumber(table, 0, 0, 9));
-transferNumbers(table, 9);
+displayTable(table, 9);
